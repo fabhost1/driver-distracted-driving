f6a9f211a0a8769164fff410c3b8baf9ad6b14bd
unknown <pythonfabhost2021@gmail.com>
2022-03-25 12:36:36 +0530

    first commit

---
+++ test (2).py
@@ -0,0 +1,91 @@
+### ---------- Handle Command Line Arguments ----------
+
+import argparse
+
+hide_img = False # default is to display image after predictions
+
+a = argparse.ArgumentParser(description="Predict the class of a given driver image.")
+a.add_argument("--image", help="path to image", default='')
+a.add_argument("--hide_img", action="store_true", help="do NOT display image on prediction termination")
+args = a.parse_args()
+
+if args.hide_img:
+    hide_img = True
+    
+if args.image is not None:
+    img_path = args.image
+
+### ---------- Import Relevant Libraries ----------
+
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from model import create_model
+import numpy as np
+import operator
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import cv2
+### ---------- Predict Image Class ----------
+cap=cv2.VideoCapture("44.mp4")
+count = 0
+while True:
+    ret,frame=cap.read()
+    z=cap.get(10)
+    cv2.imshow("frame",frame)
+    cv2.resize(frame,(100,100))
+    
+    #image=cv2.
+    cv2.imwrite("out.jpg" , frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+    img_path ="out.jpg"
+    class_labels = ['safe_driving', 'texting_right', 'talking_on_phone_right', 'texting_left', 'talking_on_phone_left',
+                    'operating_radio', 'drinking', 'reaching_behind', 'doing_hair_makeup', 'talking_to_passanger']
+
+    model = create_model()
+    model.load_weights("_weights.h5")
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    target_size=(150,150)
+
+    # prepare image for classification using keras utility functions
+    image = load_img(img_path, target_size=target_size)
+
+    image_arr = img_to_array(image) # convert from PIL Image to NumPy array
+    # the dimensions of image should now be (150, 150, 3)
+
+    # to be able to pass it through the network and use batches, we want it with shape (1, 150, 150, 3)
+    image_arr = np.expand_dims(image_arr, axis=0)
+    image_arr /= 255
+
+    # classify given an image
+    predictions = model.predict(image_arr)
+
+    # get human-readable labels of the preditions, as well as the corresponding probability
+    decoded_predictions = dict(zip(class_labels, predictions[0]))
+
+    # sort dictionary by value
+    decoded_predictions = sorted(decoded_predictions.items(), key=operator.itemgetter(1), reverse=True)
+
+    print()
+   
+    for key, value in decoded_predictions[:5]:
+        print("{}. {}: {:8f}%".format(count, key, value*100))
+        count+=1
+
+    #if not hide_img:
+        # print image
+    #    plt.imshow(image)
+    #    plt.axis('off')
+    #    plt.show()
+    count +=1
+cap.release()
+
+cv2.destroyAllWindows()
